837cc2d460c5af3685a7bf6b964215f9be19926a
Kevin Kamanda <kevkamanda@gmail.com>
2019-03-19 18:04:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,27 +1,6 @@
-katz_deli = [ ]
+katz_deli = []
 
 def line 
-  
-  if katz_deli.size = 0 
-    puts "The line is currently empty."
-  else 
-    puts "The line"
-end
-
-def take_a_number(katz_deli, name)
-  
-  puts "Welcome #{name}. You are number" katz_deli.insert(katz_deli, -1) "in line."
-  katz_deli.push(name) OR katz_deli << (name)
-  
-  katz_deli.each_with_index do |katz_deli, name|
-  
-end
-
-def now_serving(name)
-  
-  counter = 1
-  puts "Currently serving #{name}."
-  
   
   
   
